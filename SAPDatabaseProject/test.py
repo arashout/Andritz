656e395a377d0458e7f7e8a4370cf21958e54852
Outadi Arash <Arash.Outadi@andritz.com>
2016-10-11 12:08:36 -0700

    Create a material class and add function read csv files created from SAP

---
+++ SAPDatabaseProject/test.py
@@ -0,0 +1,4 @@
+from material import create_materials_from_SAP_csv, Material
+#File path to SAP search files
+file_path = r"C:\Users\delara01\Documents\SAP\SAPSearches\hex nut.csv"
+list_stuff = create_materials_from_SAP_csv(file_path)
