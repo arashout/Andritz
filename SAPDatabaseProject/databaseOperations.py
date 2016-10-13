e4556c97ebea8e4bc15f4ee4e708a5753e61098f
Arash Outadi <arash.out@gmail.com>
2016-10-13 07:47:52 -0700

    Added a template file for setuping up databse using pure SQL

---
+++ SAPDatabaseProject/databaseOperations.py
@@ -0,0 +1,25 @@
+'''
+This module contains functions relating to the mySQL database,
+especially concerning setup and termination
+'''
+#TODO
+#Implement the mysql connector
+
+def setup_AndritzDB():
+	'''
+	CREATE DATABASE AndritzCoop;
+	'''
+
+
+def use_AndritzDB():
+	'''
+	USE DATABASE AndritzCoop;
+	'''
+
+
+def setup_SAP_material_table():
+	'''
+	CREATE TABLE sap_materials(
+
+	)
+	'''
--- SAPDatabaseProject/material.py
+++ SAPDatabaseProject/material.py
@@ -1,22 +1,23 @@
 class Material(object):
-	def __init__(self, list_info):
-		'''
-		Assuming that list_info is fed from parse_SAP_export
-		'''
-		self.mat_num = list_info[2]
-		self.mat_type = list_info[3]
-		self.desc = list_info[5]
-		self.basic_mat = list_info[6]
-		self.amc = list_info[9]
+    def __init__(self, list_info):
+        '''
+        Assuming that list_info is fed from parse_SAP_export
+        '''
+        self.mat_num = list_info[2]
+        self.mat_type = list_info[3]
+        self.desc = list_info[5]
+        self.basic_mat = list_info[6]
+        self.amc = list_info[9]
+
 
 def create_materials_from_SAP_csv(file_path):
-	with open(file_path, 'r') as f:
-		list_rows = f.readlines()
+    with open(file_path, 'r') as f:
+        list_rows = f.readlines()
 
-	list_obj = []
-	#Skip header rows
-	for i in range(1,len(list_rows)):
-		row = list_rows[i]
-		list_obj.append(Material(row.split(',')))
+    list_obj = []
+    # Skip header rows
+    for i in range(1, len(list_rows)):
+        row = list_rows[i]
+        list_obj.append(Material(row.split(',')))
 
-	return list_obj
+    return list_obj
