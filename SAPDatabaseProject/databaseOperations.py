0fdf5a33399828d70834d3fe4d193bbb99a43ff7
Arash Outadi <arash.out@gmail.com>
2016-10-13 19:02:42 -0700

    Added some functions to module

--- SAPDatabaseProject/databaseOperations.py
+++ SAPDatabaseProject/databaseOperations.py
@@ -2,24 +2,34 @@
 This module contains functions relating to the mySQL database,
 especially concerning setup and termination
 '''
-#TODO
-#Implement the mysql connector
-
-def setup_AndritzDB():
-	'''
-	CREATE DATABASE AndritzCoop;
-	'''
+# TODO
+# Implement the mysql connector
 
 
-def use_AndritzDB():
-	'''
-	USE DATABASE AndritzCoop;
-	'''
+def setup_db(db_name):
+    '''
+    CREATE DATABASE AndritzCoop;
+    '''
+    command = "CREATE DATABASE {0}".format(db_name)
 
+
+def grant_permissions(db_name, username, host, password):
+    '''
+    GRANT ALL PRIVILEGES ON dbTest.* To 'arash'@'localhost' IDENTIFIED BY 'password';
+    '''
+    command = "GRANT ALL PRIVILEGES ON {0}.* TO '{1}'@'{2}' IDENTIFIED BY {3}"
+        .format(database_name, username, password)
+
+
+def select_db():
+    '''
+    USE DATABASE AndritzCoop;
+    '''
+    command = "USE DATABASE {0}".format(db_name)
 
 def setup_SAP_material_table():
-	'''
-	CREATE TABLE sap_materials(
+    '''
+    CREATE TABLE sap_materials(
 
-	)
-	'''
+    )
+    '''
