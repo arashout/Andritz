0b4de1271d55c552ec60819dc92369c6caa21be9
arashout <arash.out@gmail.com>
2016-10-15 19:40:36 -0700

    Added basic test case for testing connection and table creation
    This took much longer than I thought...

--- SAPDatabaseProject/Testing/test.py
+++ SAPDatabaseProject/Testing/test.py
@@ -1,20 +1,48 @@
+#  -*- coding: utf-8 -*-
+import pymysql
 import unittest
 
-class ConnectionTestCase(unittest.TestCase):
+
+class CreateTableTestCase(unittest.TestCase):
+    conn = pymysql.connect(host='localhost', port=3306,
+                           user='arash', passwd='main', db='andritz',
+                           use_unicode=True, charset="utf8")
+    cur = conn.cursor()
+
     def setUp(self):
-        pass
-    def tearDown(self):
+        '''
+        '''
         pass
 
-class InsertionTestCase(unittest.TestCase):
-    def setUp(self):
-        pass
-    def tearDown(self):
-        pass
+    def test_create_table(self):
+        '''
+        A command that setups the  test sap_materials table if it doesn't exist
+        '''
+        command = """
+        CREATE TABLE IF NOT EXISTS andritz.test_sap_materials(
+        mat_num CHAR(9) NOT NULL,
+        mat_type CHAR(4) NOT NULL,
+        description BLOB,
+        basic_mat VARCHAR(255),
+        amc CHAR(11),
+        PRIMARY KEY (mat_num)
+        )
+        """.replace('\n', '')
+        success = self.cur.execute(command)
+        # If command hits an error return value == E
+        assert(success == 0)
 
-class UpdateTestCase(unittest.TestCase):
-    def setUp(self):
-        pass
     def tearDown(self):
-        pass
+        '''
+        Method to delete test table and close all connections
+        '''
+        command = """
+        DROP TABLE IF EXISTS andritz.test_sap_materials
+        """
+        self.cur.execute(command)
+        self.cur.close()
+        self.conn.close()
 
+
+if __name__ == '__main__':
+    unittest.main()
